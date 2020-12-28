5b8fb0d52a544bc9f41ba1603cb03a2375636812
Xavier Wahyuadi <xavierwahyuadi2505@gmail.com>
2020-12-28 13:52:55 +0700

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -25,7 +25,7 @@ class UserSeeder extends Seeder
             ]
         ]);
 
-        DB::table('user')->insert([
+        DB::table('users')->insert([
             [
                 'username' => 'admin',
                 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
