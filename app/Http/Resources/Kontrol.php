6972f25fa62f899b3310043d302176bc630e9b3e
Xavier Wahyuadi <xavierwahyuadi2505@gmail.com>
2020-12-28 13:56:43 +0700

    Update Kontrol.php

--- app/Http/Resources/Kontrol.php
+++ app/Http/Resources/Kontrol.php
@@ -15,6 +15,7 @@ class Kontrol extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
             'date' => $this->date,
             'tempat_kontrol' => $this->tempat_kontrol,
