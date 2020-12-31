2b7834ba9b320ddd554684e0a3fa7418d310a6f8
Xavier Wahyuadi <xavierwahyuadi2505@gmail.com>
2020-12-31 11:14:44 +0700

    update kontrol

--- app/Http/Controllers/KontrolController.php
+++ app/Http/Controllers/KontrolController.php
@@ -53,38 +53,53 @@ class KontrolController extends Controller
             'temperature' => 'required',
             'base64_img' => 'required',
         ]);
-        $pasien = $request->user();
-        /*
+
+        $user = $request->user();
+        if ($user->user_role == 'nurse') {
+            $patient_id = $request->input('patient_id');
+        } else {
+            $patient_id = $user->userable->id;
+        }
+
+        $kontrol = Kontrol::where('patient_id', $patient_id);
+        if ($kontrol->doesntExists()) {
+            $order = 1;
+        } else {
+            $order = $kontrol->count() + 1;
+        }
+
         Kontrol::create([
-            'title' => $request->input('title'),
+            'order' => $order,
             'date' => $request->input('date'),
             'tempat_kontrol' => $request->input('tempat_kontrol'),
             'weight' => $request->input('weight'),
             'length' => $request->input('length'),
             'lingkar_kepala' => $request->input('lingkar_kepala'),
             'temperature' => $request->input('temperature'),
-            'patient_profile_id' => $pasien->userable->id,
+            'patient_profile_id' => $patient_id,
+            'note' => $request->input('note'),
+            'nurse_note' => $request->input('nurse_note'),
         ]);
-*/
-        $kontrol = new Kontrol;
-        $kontrol->title = $request->input('title');
-        $kontrol->date = $request->input('date');
-        $kontrol->tempat_kontrol = $request->input('tempat_kontrol');
-        $kontrol->weight = $request->input('weight');
-        $kontrol->length = $request->input('length');
-        $kontrol->lingkar_kepala = $request->input('lingkar_kepala');
-        $kontrol->temperature = $request->input('temperature');
-        $kontrol->patient_profile_id = $pasien->userable->id;
-        $kontrol->note = $request->input('note');
-        $kontrol->nurse_note = $request->input('nurse_note');
-        $kontrol->save();
 
-        //$kontrolId = Kontrol::orderBy('id', 'desc')->limit(1)->value('id');
+//        $kontrol = new Kontrol;
+//        $kontrol->order = $order;
+//        $kontrol->date = $request->input('date');
+//        $kontrol->tempat_kontrol = $request->input('tempat_kontrol');
+//        $kontrol->weight = $request->input('weight');
+//        $kontrol->length = $request->input('length');
+//        $kontrol->lingkar_kepala = $request->input('lingkar_kepala');
+//        $kontrol->temperature = $request->input('temperature');
+//        $kontrol->patient_profile_id = $patient_id;
+//        $kontrol->note = $request->input('note');
+//        $kontrol->nurse_note = $request->input('nurse_note');
+//        $kontrol->save();
+
+        $kontrolId = Kontrol::orderBy('id', 'desc')->limit(1)->value('id');
 
         $kontrolId = $kontrol->id;
         $image = base64_decode($request->input('base64_img'));
         $str = Str::random(10);
-        $filename = 'public/kontrol/' . (string) $kontrolId . $request->input('title') . '$' . $str;
+        $filename = 'public/kontrol/' . (string) $kontrolId . $request->input('title') . '$' . $str . '.jpg';
         Storage::put($filename, $image);
 
         Image::create([
