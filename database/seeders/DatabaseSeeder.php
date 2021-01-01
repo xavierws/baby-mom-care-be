bb6920212e3a8bf348b98b36a8a8e67d5b786b5c
Xavier Wahyuadi <xavierwahyuadi2505@gmail.com>
2021-01-01 09:52:00 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,8 @@ class DatabaseSeeder extends Seeder
             HospitalSeeder::class,
             RoleSeeder::class,
             UserSeeder::class,
-            CategorySeeder::class
+            CategorySeeder::class,
+            TopicSeeder::class,
         ]);
     }
 }
