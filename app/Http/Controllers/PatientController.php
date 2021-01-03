8391490a15283e487f841fe119fbd1d80a526bf5
Xavier Wahyuadi <xavierwahyuadi2505@gmail.com>
2021-01-04 03:46:52 +0700

    Update PatientController.php

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -6,8 +6,10 @@ use Illuminate\Http\Request;
 
 class PatientController extends Controller
 {
-    public function listNurse()
+    public function listNurse(Request $request)
     {
+        $patient = $request->user()->userable;
 
+        return response($patient->nurses);
     }
 }
