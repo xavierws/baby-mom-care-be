b25a3d7cb048991f3dc330177ce41c3b8a7e01f2
Xavier Wahyuadi <xavierwahyuadi2505@gmail.com>
2021-01-04 02:31:15 +0700

    fixing

--- app/Http/Resources/PatientProfile.php
+++ app/Http/Resources/PatientProfile.php
@@ -14,6 +14,27 @@ class PatientProfile extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'baby_name' => $this->baby_name,
+            'baby_birthday' => $this->baby_birthday,
+            'born_weight' => $this->born_weight,
+            'born_length' => $this->born_length,
+            'current_weight' => $this->current_weight,
+            'current_length' => $this->current_length,
+            'baby_gender' => $this->baby_gender,
+            'mother_name' => $this->mother_name,
+            'mother_birthday' => $this->mother_birthday,
+            'mother_religion' => $this->mother_religion,
+            'mother_education' => $this->mother_education,
+            'mother_job' => $this->mother_job,
+            'paritas' => $this->paritas,
+            'father_name' => $this->father_name,
+            'father_birthday' => $this->father_birthday,
+            'father_religion' => $this->father_religion,
+            'father_education' => $this->father_education,
+            'father_job' => $this->father_job,
+            'status' => $this->status,
+            'materi' => $this->materis,
+        ];
     }
 }
