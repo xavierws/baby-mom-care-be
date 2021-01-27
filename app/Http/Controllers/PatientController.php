791bda46529d2b43eda48783cf725200e034f442
okazaki10 <rakaabadisusilo@gmail.com>
2021-01-27 15:22:09 +0700

    Update PatientController.php

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -22,34 +22,41 @@ class PatientController extends Controller
 
     public function update(Request $request)
     {
-//        $user = $request->user();
+        //        $user = $request->user();
 
         $patient = PatientProfile::find($request->id);
-        $patient->baby_name = $request->baby_name;
-        $patient->baby_birthday = $request->baby_birthday;
-        $patient->born_weight = $request->born_weight;
-        $patient->born_length = $request->born_length;
-        $patient->lingkar_kepala = $request->lingkar_kepala;
-        $patient->baby_gender = $request->baby_gender;
-        $patient->usia_gestas = $request->usia_gestas;
-        $patient->harapan_orangtua = $request->harapan_orangtua;
-        $patient->mother_name = $request->mother_name;
-        $patient->mother_birthday = $request->mother_birthday;
-        $patient->mother_religion = $request->mother_religion;
-        $patient->mother_education = $request->mother_education;
-        $patient->mother_job = $request->mother_job;
-        $patient->paritas = $request->paritas;
-        $patient->jumlah_anak = $request->jumlah_anak;
-        $patient->pengalaman_merawat = $request->pengalaman_merawat;
-        $patient->tinggal_dengan_suami = $request->tinggal_dengan_suami;
-        $patient->father_name = $request->father_name;
-        $patient->father_birthday = $request->father_birthday;
-        $patient->father_name = $request->father_name;
-        $patient->father_religion = $request->father_religion;
-        $patient->father_education = $request->father_education;
-        $patient->father_job = $request->father_job;
-        $patient->pendapatan_keluarga = $request->pendapatan_keluarga;
-        $patient->phone = $request->phone;
+        if ($request->baby_name) {
+            $patient->baby_name = $request->baby_name;
+            $patient->baby_birthday = $request->baby_birthday;
+            $patient->born_weight = $request->born_weight;
+            $patient->born_length = $request->born_length;
+            $patient->lingkar_kepala = $request->lingkar_kepala;
+            $patient->baby_gender = $request->baby_gender;
+            $patient->usia_gestas = $request->usia_gestas;
+            $patient->harapan_orangtua = $request->harapan_orangtua;
+        }
+        if ($request->mother_name) {
+            $patient->mother_name = $request->mother_name;
+            $patient->mother_birthday = $request->mother_birthday;
+            $patient->mother_religion = $request->mother_religion;
+            $patient->mother_education = $request->mother_education;
+            $patient->mother_job = $request->mother_job;
+            $patient->paritas = $request->paritas;
+            $patient->jumlah_anak = $request->jumlah_anak;
+            $patient->pengalaman_merawat = $request->pengalaman_merawat;
+            $patient->tinggal_dengan_suami = $request->tinggal_dengan_suami;
+            $patient->father_name = $request->father_name;
+            $patient->father_birthday = $request->father_birthday;
+            $patient->father_name = $request->father_name;
+            $patient->father_religion = $request->father_religion;
+            $patient->father_education = $request->father_education;
+            $patient->father_job = $request->father_job;
+            $patient->pendapatan_keluarga = $request->pendapatan_keluarga;
+        }
+        if ($request->phone) {
+            $patient->phone = $request->phone;
+        }
+
         $patient->save();
 
         return response()->json([
