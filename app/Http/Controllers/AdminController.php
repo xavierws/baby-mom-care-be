247c2ffd3236d7f8b735774868b81144e0c49b6b
okazaki10 <rakaabadisusilo@gmail.com>
2021-02-02 21:04:11 +0700

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -343,7 +343,7 @@ class AdminController extends Controller
 
     public function showUserLog()
     {
-        $userLog = UserLog::all()->orderBy('created_at', 'desc');
+        $userLog = UserLog::orderBy('created_at', 'desc')->get();
 
         return UserLogRes::collection($userLog);
     }
