77c5ac278770ccad12a0c71f67bbefedbe245329
Xavier Wahyuadi <xavierwahyuadi05@gmail.com>
2021-03-29 19:53:32 +0700

    adding handle for empty data

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -228,35 +228,39 @@ class AdminController extends Controller
 
         $color = ['#00f7ff', '#ff0000', '#ffd500', '#1bb525', '#1957bd'];
         $data = array();
-        foreach ($surveys as $survey) {
-            foreach ($survey->questions as $question) {
-                for ($i = 0; $i<=4; $i++) {
-                    $count = DB::table('patient_survey')->where([
+
+        if ($surveys) {
+            foreach ($surveys as $survey) {
+                foreach ($survey->questions as $question) {
+                    for ($i = 0; $i<=4; $i++) {
+                        $count = DB::table('patient_survey')->where([
 //                        ['survey_id', $survey->id],
-                        ['question_id', $question->id],
-                        ['answer', $i+1]
-                    ])->count();
+                            ['question_id', $question->id],
+                            ['answer', $i+1]
+                        ])->count();
 
-                    $data[$i] = [
-                        'name' => $i+1,
-                        'choice_type' => $survey->choice_type,
-                        'count' => $count,
-                        'color' => $color[$i],
-                        'legendFontColor'=> "#7F7F7F",
-                        'legendFontSize'=> 15
+                        $data[$i] = [
+                            'name' => $i+1,
+                            'choice_type' => $survey->choice_type,
+                            'count' => $count,
+                            'color' => $color[$i],
+                            'legendFontColor'=> "#7F7F7F",
+                            'legendFontSize'=> 15
+                        ];
+
+                    }
+                    $data2[] = [
+                        'question' => $question->question,
+                        'jawaban' => $data
                     ];
-
                 }
-                $data2[] = [
-                    'question' => $question->question,
-                    'jawaban' => $data
+                $data3[] = [
+                    'survey' => $survey->title,
+                    'pertanyaan' => $data2
                 ];
             }
-            $data3[] = [
-                'survey' => $survey->title,
-                'pertanyaan' => $data2
-            ];
         }
+
 
         return response($data3);
     }
