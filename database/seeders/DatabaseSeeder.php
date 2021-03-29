c4f4ce33006c02c1b1dd32dc57cc8078cf8610e7
Xavier Wahyuadi <xavierwahyuadi05@gmail.com>
2021-03-29 19:52:38 +0700

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
             TopicSeeder::class,
             ChatSeeder::class,
             SurveySeeder::class,
+            MateriSeeder::class
         ]);
     }
 }
