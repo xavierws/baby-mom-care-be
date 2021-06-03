bc420fb0c2bd9822a3213d542b32025c7c874a98
Xavier Wahyuadi <xavierwahyuadi05@gmail.com>
2021-06-03 08:44:36 +0700

    Update UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,55 +16,71 @@ class UserSeeder extends Seeder
     public function run()
     {
         DB::table('nurse_profiles')->insert([
+            // [
+            //     'name' => 'admin',
+            //     'working_exp' => 1,
+            //     'education' => 'S1',
+            //     'phone' => '089765432100',
+            //     'hospital_id' => 1,
+            //     'is_approved' => true,
+            // ],
+            // [
+            //     'name' => 'super_admin',
+            //     'working_exp' => 1,
+            //     'education' => 'S1',
+            //     'phone' => '089765432100',
+            //     'hospital_id' => 1,
+            //     'is_approved' => true,
+            // ],
+            // [
+            //     'name' => 'Raka',
+            //     'working_exp' => 1,
+            //     'education' => 'S1',
+            //     'phone' => '089765432100',
+            //     'hospital_id' => 1,
+            //     'is_approved' => true,
+            // ]
             [
-                'name' => 'admin',
+                'name' => 'kristiawati',
                 'working_exp' => 1,
                 'education' => 'S1',
-                'phone' => '089765432100',
-                'hospital_id' => 1,
-                'is_approved' => true,
-            ],
-            [
-                'name' => 'super_admin',
-                'working_exp' => 1,
-                'education' => 'S1',
-                'phone' => '089765432100',
-                'hospital_id' => 1,
-                'is_approved' => true,
-            ],
-            [
-                'name' => 'Raka',
-                'working_exp' => 1,
-                'education' => 'S1',
-                'phone' => '089765432100',
+                'phone' => '081334746022',
                 'hospital_id' => 1,
                 'is_approved' => true,
             ]
         ]);
 
         DB::table('users')->insert([
+            // [
+            //     'username' => 'admin',
+            //     'password' => Hash::make('admin-123'),
+            //     'role_id' => 21,
+            //     'email' => null,
+            //     'userable_id' => 1,
+            //     'userable_type' => 'App\Models\NurseProfile'
+            // ],
+            // [
+            //     'username' => 'super_admin',
+            //     'password' => Hash::make('admin-123'),
+            //     'role_id' => 22,
+            //     'email' => null,
+            //     'userable_id' => 2,
+            //     'userable_type' => 'App\Models\NurseProfile'
+            // ],
+            // [
+            //     'username' => 'raka',
+            //     'password' => Hash::make('admin-123'),
+            //     'role_id' => 20,
+            //     'email' => null,
+            //     'userable_id' => 3,
+            //     'userable_type' => 'App\Models\NurseProfile'
+            // ]
             [
-                'username' => 'admin',
-                'password' => Hash::make('admin-123'),
-                'role_id' => 21,
-                'email' => null,
-                'userable_id' => 1,
-                'userable_type' => 'App\Models\NurseProfile'
-            ],
-            [
-                'username' => 'super_admin',
+                'username' => 'kristiawati',
                 'password' => Hash::make('admin-123'),
                 'role_id' => 22,
                 'email' => null,
-                'userable_id' => 2,
-                'userable_type' => 'App\Models\NurseProfile'
-            ],
-            [
-                'username' => 'raka',
-                'password' => Hash::make('admin-123'),
-                'role_id' => 20,
-                'email' => null,
-                'userable_id' => 3,
+                'userable_id' => 6,
                 'userable_type' => 'App\Models\NurseProfile'
             ]
         ]);
