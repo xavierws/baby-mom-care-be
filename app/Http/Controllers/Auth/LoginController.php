0484230d2d41cea922454f4b2c80d7541c463640
Xavier Wahyuadi <xavierwahyuadi05@gmail.com>
2021-06-25 17:14:41 +0700

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -39,6 +39,11 @@ class LoginController extends Controller
         $this->middleware('guest')->except('logout');
     }
 
+    public function username()
+    {
+        return 'username';
+    }
+
     // public function login(Request $request)
     // {
     //     $request->validate([
