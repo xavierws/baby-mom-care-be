cfffa25811198bd54622321a7749b821ebf74983
rakaabadi <rakaabadisusilo@gmail.com>
2021-07-10 09:05:42 +0700

    Update PatientController.php

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -36,7 +36,7 @@ class PatientController extends Controller
             $patient->baby_gender = $request->baby_gender;
             $patient->usia_gestas = $request->usia_gestas;
             $patient->harapan_orangtua = $request->harapan_orangtua;
-            $request->hospital_entry = $request->hospital_entry;
+            $patient->hospital_entry = $request->hospital_entry;
         }
         if ($request->mother_name) {
             $patient->mother_name = $request->mother_name;
