1b137a24ebce561ab4467352aa001041847dd9a7
rakaabadi <rakaabadisusilo@gmail.com>
2021-07-29 23:06:31 +0700

    Update SurveyController.php

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -157,7 +157,7 @@ class SurveyController extends Controller
             if ($order > 3) {
                 return response()->json([
                     'message' => 'patient already fill 3 times',
-                    'error'=>'error'
+                    'error'=>1
                 ]);
             }
 
