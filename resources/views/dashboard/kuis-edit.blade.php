07b77d270e991b7d60e3502f004221aee1133caf
Xavier Wahyuadi <xavierwahyuadi05@gmail.com>
2021-09-15 20:18:21 +0700

    Update kuis-edit.blade.php

--- resources/views/dashboard/kuis-edit.blade.php
+++ resources/views/dashboard/kuis-edit.blade.php
@@ -18,7 +18,6 @@
                         @method('PUT')
 
                         <div class="d-none">{{ $i = 1 }}</div>
-                        <div class="d-none">{{ $r=['A','B','C'] }}</div>
                         <div class="question">
                             @foreach ($questions as $q)
                             <div class="card">
@@ -31,7 +30,7 @@
                                             <p class="d-none">{{ $trueAnswer = null }}</p>
                                             @forelse ($q->choices as $c)
                                                 <div class="col">
-                                                    <label for="{{ 'choice['.$i.']['.$j.']'}}">pilihan {{ $r[$j-1] }}</label>
+                                                    <label for="{{ 'choice['.$i.']['.$j.']'}}">pilihan {{ $j }}</label>
                                                     <textarea type="text" name="{{ 'choice['.$i.']['.$j.']'}}" placeholder="pilihan 1" class="form-control" required>{{ $c->choice }}</textarea>
                                                     <p class="d-none">{{ $trueAnswer = $c->is_true==1? $j:$trueAnswer }}</p>
                                                     <p class="d-none">{{ $j++ }}</p>
